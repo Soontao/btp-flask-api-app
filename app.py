54a0f007e14fa77ff2c6d38f9c4680f661303e82
Theo Sun <theo.sun.github@outlook.com>
2023-11-25 07:21:43 +0000

    chore: update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,g
+from flask import Flask, request, g
 from sap import xssec
 from cfenv import AppEnv
 
@@ -6,22 +6,24 @@ app = Flask(__name__)
 app.config.from_prefixed_env()
 env = AppEnv()
 
+
 @app.before_request
 def authentication():
-    uaa_service = env.get_service(label='xsuaa')
+    uaa_service = env.get_service(label="xsuaa")
     auth_header = request.headers.get("Authorization")
     if not auth_header:
-        return {'error': 'Token is missing'}, 401
+        return {"error": "Token is missing"}, 401
 
     # Extract the token from the Authorization header
     try:
-        token_type, token = auth_header.split(' ')
-        if token_type.lower() != 'bearer':
-            raise ValueError('Invalid token type')
+        token_type, token = auth_header.split(" ")
+        if token_type.lower() != "bearer":
+            raise ValueError("Invalid token type")
     except ValueError:
-        return {'error': 'Invalid token format'}, 401
-    
+        return {"error": "Invalid token format"}, 401
+
     g.security_context = xssec.create_security_context(token, uaa_service.credentials)
+
 
 @app.route("/")
 def index():
